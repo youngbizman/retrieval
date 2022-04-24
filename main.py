bb68232bb7b9289c7e28650ff8269417ecc7f2ee
shima <shimaamir2002@gmail.com>
2022-04-24 14:13:10 +0430

    hgfvgbhn

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+from __future__ import unicode_literals
+from hazm import *
+import tqdm
+import random
+import numpy as np
+import nltk
+import pandas as pd
+import codecs
+
 
 def main():
-    raise NotImplemented
+    file = open("input.txt", "r", encoding='utf-8')
+    file_contents = file.read()
+
+    normalizer = Normalizer()
+    normalized = normalizer.normalize(file_contents)
+    sentences = sent_tokenize(normalized)
+    # for sentence in sentences:
+    #     if sentence.startswith('مواد لازم'):
+    #
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
