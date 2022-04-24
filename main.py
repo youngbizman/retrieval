d70a997a66918ed10bbf2e57dbbcf83ee5066bb4
shima <shimaamir2002@gmail.com>
2022-04-24 18:31:57 +0430

    avalie2

--- main.py
+++ main.py
@@ -28,8 +28,32 @@ def main():
 
     i = find_word(sentencess, 'مواد لازم')
     j = find_word(sentencess[i:], 'طرز تهیه')
-    print(sentencess[i+1:j])
-    print(*sentencess[j+1:])
+    # for t in range(i+1, j):
+    #     sentencess[t].find
+    # print(sentencess[i+1:j])
+    # print(*sentencess[j+1:])
+    separators = [normalizer.normalize(x.strip()) for x in codecs.open('separators.txt', 'r', 'utf-8').readlines()]
+    print(separators)
+    num = ['۱', '۲', '۳','۴','۵','۶','۷','۸','۹']
+    for t in range(i+1, j):
+
+        z = len(sentencess[t][0])
+        if sentencess[t][0][0] in num:
+            sentencess[t][0]= sentencess[t][0].split(maxsplit=1)[1]
+        # if sentencess[t][0].startswith():
+
+        # z = 0
+        for separator in separators:
+            curr_index = sentencess[t][0].find(separator)
+            # z = max(z, curr_index)
+
+            if curr_index != -1 and curr_index < z:
+                z = curr_index
+
+
+
+        print(sentencess[t][0][:z])
+        print(sentencess[t][0][z:])
 
 
 
