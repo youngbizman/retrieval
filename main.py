38e8507fb70f65eac79150493b571e0d58d0505b
shima <shimaamir2002@gmail.com>
2022-04-24 14:45:11 +0430

    avalie

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import nltk
 import pandas as pd
 import codecs
 
+def find_word(list, word):
+    for i in range(len(list)):
+        if list[i][0].startswith(word):
+            return i
+
+
 
 def main():
     file = open("input.txt", "r", encoding='utf-8')
@@ -15,10 +21,20 @@ def main():
     normalizer = Normalizer()
     normalized = normalizer.normalize(file_contents)
     sentences = sent_tokenize(normalized)
+    sentencess = []
+    for sentence in sentences:
+        sentencess.append(sentence.split('\n'))
+
+
+    i = find_word(sentencess, 'مواد لازم')
+    j = find_word(sentencess[i:], 'طرز تهیه')
+    print(sentencess[i+1:j])
+    print(*sentencess[j+1:])
+
+
+
     # for sentence in sentences:
     #     if sentence.startswith('مواد لازم'):
-
-
 
 
 
