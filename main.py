7f0c833506661972cb5db6764923e01bdb73209e
shima <shimaamir2002@gmail.com>
2022-04-25 22:37:04 +0430

    third edition

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from __future__ import unicode_literals
 from hazm import *
-import tqdm
-import random
-import numpy as np
-import nltk
-import pandas as pd
 import codecs
+import timeit
+import re
 
 
 def find_word(list, word):
@@ -14,6 +11,7 @@ def find_word(list, word):
         if list[i].find(word) != -1 and len(list[i]) < 40:
             result.append(i)
     return result
+
 
 def find_word2(list, word):
     result = []
@@ -32,12 +30,29 @@ def find_next(list, m):
     return first_value
 
 
+def set_default(sentencess):
+    ''' find the first line with more than 40 characters '''
+
+    i = 0
+    while len(sentencess[i]) < 40:
+        i += 1
+    if i == len(sentencess):
+        return []
+    else:
+        return i
+
+
 def main():
     file = open("input.txt", "r", encoding='utf-8')
     file_contents = file.read()
+
     sentencess = file_contents.split('\n')
+    # "to remove empty lines"
+    # sentencess = list(filter(None, sentencess))
     file.close()
 
+    result = {"ingredients": [], "quantity": [], "recipie": [], "span_ingredients": [], "span_recipie": []}
+    start = timeit.default_timer()
 
     normalizer = Normalizer()
     separators = [normalizer.normalize(x.strip()) for x in codecs.open('separators.txt', 'r', 'utf-8').readlines()]
@@ -48,32 +63,51 @@ def main():
     for preparator in preparators:
         l2 += find_word2(sentencess, preparator)
 
+    if len(l2) == 0:
+        l2.append(set_default(sentencess))
+
     l3 = l1 + l2
     for i in l2:
         next = find_next(l3, i)
-        print(sentencess[i: next])
 
+        recipe = "\n".join(sentencess[i: next])
+        loc = file_contents.index(recipe)
+        result["span_recipie"].append([loc, loc + len(recipe)])
+
+        result["recipie"].append(recipe.replace("\n", ""))
 
     num = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
 
     for j in l1:
         next = find_next(l3, j)
-        # print(sentencess[j+1:next])
 
-        for t in range(j+1, next):
+        for t in range(j + 1, next):
 
             z = len(sentencess[t])
+            if z == 0:
+                continue
+
             if sentencess[t] in num:
                 sentencess[t] = sentencess[t].split(maxsplit=1)[1]
 
             for separator in separators:
                 curr_index = sentencess[t].find(separator)
+                '''اگر اشتباهی مچ کرده بود(مثلا دو در دوغ رو)'''
+                if sentencess[t][curr_index-1] != ' ' and sentencess[t][curr_index+1] != ' ':
+                    continue
 
                 if curr_index != -1 and curr_index < z:
                     z = curr_index
 
-            print(sentencess[t][:z])
-            print(sentencess[t][z:])
+            loc = file_contents.index(sentencess[t])
+            result["span_ingredients"].append([loc, loc + len(sentencess[t])])
+
+            result["ingredients"].append(re.sub(r'^[\W_]+|[\W_]+$', '', sentencess[t][:z]))
+            result["quantity"].append(sentencess[t][z:])
+
+    stop = timeit.default_timer()
+    result["time"] = stop - start
+    print(result)
 
 
 if __name__ == '__main__':
